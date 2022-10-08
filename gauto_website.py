2e147fdac5c72ac07eb031ffbb91499c236ed969
Jhithan <jonathankpangilinanjr@gmail.com>
2022-10-08 19:27:11 +0800

    Forgot

--- gauto_website.py
+++ gauto_website.py
@@ -10,5 +10,9 @@ def homepage():
 def grindpage():
     return render_template('grindpage.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
